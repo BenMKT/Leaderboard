1ab74a73f69b07ff31cda6ceb77628f94f5e1fb5
Benson Kiai <benson.kiai86@gmail.com>
2023-07-28 04:00:53 +0300

    Fix ESlinter errors

--- src/index.js
+++ src/index.js
@@ -8,33 +8,32 @@ const table = document.querySelector('.table_container');
 const form = document.querySelector('.form');
 
 const getScores = async () => {
-  
-    const response = await fetch(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BenKGame/scores',
-    );
-    const json = await response.json();
-    leaderboard = json.result;
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BenKGame/scores'
+  );
+  const json = await response.json();
+  leaderboard = json.result;
 
-    const uniqueScores = new Set();
-    // Loop through the leaderboard array and add each user and score pair to the Set
-    leaderboard.forEach((dat) => {
-      uniqueScores.add(`${dat.user}: ${dat.score}`);
-    });
-    // Convert the Set back to an array
-    leaderboard = Array.from(uniqueScores);
-    // Sort the leaderboard based on scores in descending order
-    leaderboard.sort((a, b) => b.split(': ')[1] - a.split(': ')[1]);
-    // console.log(leaderboard);
-    table.innerHTML = '';
-    leaderboard.forEach((entry) => {
-      const row = document.createElement('tr');
-      const name = document.createElement('td');
-      name.className = 'point';
-      name.textContent = entry;
-      row.appendChild(name);
-      table.appendChild(row);
-    });
-  } 
+  const uniqueScores = new Set();
+  // Loop through the leaderboard array and add each user and score pair to the Set
+  leaderboard.forEach((dat) => {
+    uniqueScores.add(`${dat.user}: ${dat.score}`);
+  });
+  // Convert the Set back to an array
+  leaderboard = Array.from(uniqueScores);
+  // Sort the leaderboard based on scores in descending order
+  leaderboard.sort((a, b) => b.split(': ')[1] - a.split(': ')[1]);
+  // console.log(leaderboard);
+  table.innerHTML = '';
+  leaderboard.forEach((entry) => {
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    name.className = 'point';
+    name.textContent = entry;
+    row.appendChild(name);
+    table.appendChild(row);
+  });
+};
 
 const addScore = async (userName, userScore) => {
   if (!userName || !userScore) {
@@ -42,26 +41,26 @@ const addScore = async (userName, userScore) => {
     return;
   }
 
-    const response = await fetch(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BenKGame/scores',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          user: userName,
-          score: Number(userScore),
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BenKGame/scores',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userName,
+        score: Number(userScore),
+      }),
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
-    const json = await response.json();
-    if (json.response === 'Leaderboard score created.') {
-      getScores();
     }
-    form.reset();
-    // user.value = '';
-    // score.value = '';
+  );
+  const json = await response.json();
+  if (json.response === 'Leaderboard score created.') {
+    getScores();
+  }
+  form.reset();
+  // user.value = '';
+  // score.value = '';
 };
 
 document.querySelector('.refresh').addEventListener('click', () => {
